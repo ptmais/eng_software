7fe7b2235f3dc24ec6aab415dba3dc1f30441fbf
carlo <carlo@10.144.88.3>
2016-11-04 23:40:36 -0300

    Modificação das classes para ficar de acordo com o novo diagrama

--- test/junittestes/Teste_Investimento.java
+++ test/junittestes/Teste_Investimento.java
@@ -11,18 +11,19 @@ import static org.junit.Assert.*;
  * @author Alenxadre Andrade
  */
 public class Teste_Investimento {
-    
+
     Investimento investimento;
-    
+
     public Teste_Investimento() {
         investimento = new Investimento("nome", 0, null, 0);
     }
-    
+
     @Test
-    public void cadastrarTipo(){
-        assertTrue(investimento.cadastrarTipoInvest());
+    public void cadastrarTipo() {
+        assertTrue(investimento.cadastrarInvestimento());
     }
-    
+
+    /*
     @Test
     public void cadastrarTaxa(){
         assertTrue(investimento.cadastrarTaxa());
@@ -32,5 +33,5 @@ public class Teste_Investimento {
     public void testarLucroLCI(){
         assertEquals(0, investimento.lucroInvestimento(investimento));
     }
-    
+     */
 }
